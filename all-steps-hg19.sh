c31c3df4b8ea478c4c523aa2d47f1703fddc6f59
Hernan Lorenzi <Hernan.Lorenzi@nih.gov>
2022-10-28 14:17:56 -0400

    fixing variable export

--- all-steps-hg19.sh
+++ all-steps-hg19.sh
@@ -145,7 +145,7 @@ done < $INPUTFILE_1 3<$INPUTFILE_2 > $LOGFILE 2>&1
 echo ""
 echo ""
 echo "### Collecting new splice sites in *SJ.out.tab files"
-newsjdb=$(find ./04-mapping-hg19 -type f -name "*SJ.out.tab"|perl -e 'while(<>){chomp;s/^\.\///;push @x, $_};print join(" ",@x)')
+newsjdb=$(find ${AD}/04-mapping-hg19 -type f -name "*SJ.out.tab"|perl -e 'while(<>){chomp;push @x, $_};print join(" ",@x)')
 export newsjdb
 
 # analyse each file from the input list
